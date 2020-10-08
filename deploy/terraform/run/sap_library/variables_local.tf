29386190eac2aca37258cd436b08307805d0f017
Yunzi Zhang <yunzzha@microsoft.com>
2020-10-08 09:53:05 -0700

    add an output to test

--- deploy/terraform/run/sap_library/variables_local.tf
+++ deploy/terraform/run/sap_library/variables_local.tf
@@ -62,9 +62,9 @@ locals {
   deployer_key_vault_arm_id = try(data.terraform_remote_state.remote_deployer.outputs.deployer_kv_user_arm_id, "")
 
   saplib_resource_group_name   = local.rg_name
-  tfstate_storage_account_name = try(local.deployer.tfstate_storage_account_name, "")
+  tfstate_storage_account_name = local.deployer.tfstate_storage_account_name
   tfstate_container_name       = "tfstate"
-  deployer_tfstate_key         = try(format("%s%s", local.deployer.deployer_rg_name, ".terraform.tfstate"), "")
+  deployer_tfstate_key         = format("%s%s", local.deployer_rg_name, ".terraform.tfstate")
 
   spn = {
     subscription_id = data.azurerm_key_vault_secret.subscription_id.value,
